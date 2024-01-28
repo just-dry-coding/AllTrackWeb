4cc82abc9b343d349e686566a9b747ac7717213b
justin <justin.gruenenwald@lexxpluss.com>
2024-01-28 21:04:54 +0900

    adding dialog box, moving list to parent, introduce ApplicationModes

---
+++ all_track/src/components/ButtonBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="button-container">
+    <Button
+      class="button"
+      :class="{ active: activeButton === ApplicationMode.ENTER }"
+      label="Enter"
+      @click="onClick(ApplicationMode.ENTER)"
+    />
+    <Button
+      class="button"
+      :class="{ active: activeButton === ApplicationMode.VIEW }"
+      label="View"
+      @click="onClick(ApplicationMode.VIEW)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import Button from 'primevue/button'
+import { ApplicationMode } from '@/enums/ApplicationMode'
+
+const emit = defineEmits(['modeChanged'])
+let activeButton = ref(ApplicationMode.ENTER)
+
+const onClick = (currentMode: ApplicationMode) => {
+  activeButton.value = currentMode
+  emit('modeChanged', currentMode)
+}
+</script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  background-color: #ffffff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.button {
+  width: 120px;
+  height: 55px;
+  background-color: #acacac;
+}
+
+.active {
+  background-color: #2d2d2d;
+}
+</style>
--- all_track/src/components/ButtonList.vue
+++ all_track/src/components/ButtonList.vue
@@ -1,25 +1,36 @@
 <template>
-    <div class="button-container" v-for="item of items" :key="item">
-       <Button class="button" :label="item" rounded/> 
-    </div>
+  <div class="button-container" v-for="item of items" :key="item">
+    <Button class="button" :label="item" rounded />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Button from 'primevue/button'
+// get these items here from the database
+// maybe only a table with the names of the items
+// in the next view we need to fetch the corresponding metrics to the item
 
-let items = ["some", "more", "nonesense"]
+const props = defineProps({
+  items: {
+    type: Array  as () => string[],
+    required: true,
+    default: () => ([]),
+  },
+})
+
+let items = props.items
 </script>
 
 <style scoped>
 .button-container {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
 }
 .button {
-    flex: 1;
-    margin: 10px;
-    width: 200px;
+  flex: 1;
+  margin: 10px;
+  width: 200px;
 }
 </style>
